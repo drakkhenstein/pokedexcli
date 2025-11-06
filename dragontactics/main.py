126c941052fe1de477d5c77bdeef84357d3e9e8a
drakkhenstein <greenknight84@hotmail.com>
2025-11-06 16:17:47 -0500

    Added Enemy and Battlefield sprites, spawns and movements on screen

--- dragontactics/main.py
+++ dragontactics/main.py
@@ -3,6 +3,8 @@ import sys
 from constants import *
 from player import *
 from circleshape import *
+from enemy import *
+from battlefield import *
 
 def main():
     pygame.init()
@@ -12,13 +14,17 @@ def main():
     #player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    enemies = pygame.sprite.Group()
     #asteroids = pygame.sprite.Group()
     #shots = pygame.sprite.Group()
     #bullets = pygame.sprite.Group()
-    
+
+    Enemy.containers = (enemies, updatable, drawable)
     #Asteroid.containers = (asteroids, updatable, drawable)
     #Shot.containers = (shots, updatable, drawable)
     
+    Battlefield.containers = updatable
+    battlefield = Battlefield()
     #AsteroidField.containers = updatable
     #asteroid_field = AsteroidField()
 
@@ -31,22 +37,37 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        #pause = False
 
         updatable.update(dt)
 
-        for entity in drawable:
-            entity.draw(screen)
+        #while pause is False:
+            #updatable.update(dt)
+            #for entity in drawable:
+                #entity.draw(screen)
+
+        for enemy in enemies:
+            if enemy.collision(player):
+                screen.fill("black")
+                #pause = True
+                #dt = clock.tick(0)  # seconds
+                #updatable.update(dt + 1)  # freeze the game
+                
+                #print("Game Over!")
+                #pygame.quit()
+                #sys.exit()
 
 
         pygame.display.flip()
-        dt = clock.tick(60) / 1000  # seconds
 
+        dt = clock.tick(60) / 1000  # seconds
     print ("  *** Welcome to Dragon Tactics! ***  ")
-    print (f"Screen width: {SCREEN_WIDTH}")
-    print (f"Screen height: {SCREEN_HEIGHT}")
     print ("\n")
     print ("  Use the arrow keys to move your dragon around the screen.  ")
     print ("  *** Thank you for playing! ***  ")
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+    
 
 if __name__ == "__main__":
     main()
