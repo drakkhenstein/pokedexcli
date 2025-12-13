6391ff6b9d666c9b7b681b3340cd839069e0e808
drakkhenstein <greenknight84@hotmail.com>
2025-12-13 18:36:09 -0500

    Added pokedex command prompt to repl, prints first word

--- pokedexcli/repl.go
+++ pokedexcli/repl.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan() 
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+		commandName := words[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
+}
+
 func cleanInput(text string) []string {
-	lowered := strings.ToLower(text)
-	trimmed := strings.TrimSpace(lowered)
-	if trimmed == "" {
+	lowerd := strings.ToLower(text)
+	if lowerd == "" {
 		return []string{}
 	}
+	trimmed := strings.TrimSpace(lowerd)
 	words := strings.Fields(trimmed)
 	return words
 }
