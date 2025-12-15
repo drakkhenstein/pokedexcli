d0a60312ffd1aa50e2530b9e8acef57b6fdc4663
drakkhenstein <greenknight84@hotmail.com>
2025-12-14 20:06:29 -0500

    Added commands, help and exit, command prompt functionality

--- pokedexcli/repl.go
+++ pokedexcli/repl.go
@@ -7,6 +7,28 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
 func startRepl() {
 	reader := bufio.NewScanner(os.Stdin)
 	for {
@@ -18,8 +40,29 @@ func startRepl() {
 			continue
 		}
 		commandName := words[0]
-
-		fmt.Printf("Your command was: %s\n", commandName)
+		
+		var commands = map[string]cliCommand{
+    		"exit": {
+        		name:        "exit",
+        		description: "Exit the Pokedex",
+        		callback:    commandExit,
+    		},
+			"help": {
+				name:        "help",
+				description: "Displays a help message",
+				callback:    commandHelp,
+			},
+		}
+		
+		command, exists := commands[commandName] 
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
 
